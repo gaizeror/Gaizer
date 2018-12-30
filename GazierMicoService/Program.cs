5f39c3d029c3a9e51b19786f1ce7228e094486d6
Yogev Mizrahi <yogev0mz@gmail.com>
2018-12-30 17:54:51 +0200

    Fixed Microserviceruntime

--- GazierMicoService/Program.cs
+++ GazierMicoService/Program.cs
@@ -1,10 +1,13 @@
 ﻿using MicroService4Net;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GaizerMicoService
 {
@@ -14,6 +17,20 @@ namespace GaizerMicoService
         {
             var microService = new MicroService();
             microService.Run(args);
+            //Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            //AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            // Log the exception, display it, etc
+            Debug.WriteLine(e.Exception.Message);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // Log the exception, display it, etc
+            Debug.WriteLine((e.ExceptionObject as Exception).Message);
         }
     }
 }
